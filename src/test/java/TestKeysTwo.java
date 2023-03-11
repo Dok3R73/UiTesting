23c9030bea431c6414a6f4b2fe3be633426df936
DoK3R73 <doker2273@gmail.com>
2023-03-11 04:01:56 +0300

    add page object

--- src/test/java/TestKeysTwo.java
+++ src/test/java/TestKeysTwo.java
@@ -1,9 +1,7 @@
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
